b6ae5f2985017f11d6ff726542c12dce1b46a53e
oqla-alrefai <realrefai.rr@gmail.com>
2023-08-10 08:50:18 +0000

    update done

--- backend/index.js
+++ backend/index.js
@@ -125,7 +125,7 @@ app.put("/user/:id", async (req, res) => {
     }
 
     const updateUser = await pool.query(
-      "UPDATE users SET user_id= $1,  email = $2, password = $3, full_name = $4, image_url = $5, admin = $6 WHERE user_id = $1",
+      "UPDATE users SET user_id= $1,  email = $2, password = $3, full_name = $4, image_url = $5, favorite=$6, admin = $7 WHERE user_id = $1",
       [id, email, password, name, image_url,favorite, admin]
     );
     res.json("User was updated successfully");
