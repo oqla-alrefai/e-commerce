db275efd5abc2a1dfd9a3bf77aa7ed54997a7b1c
oqla-alrefai <realrefai.rr@gmail.com>
2023-08-11 09:03:56 +0000

    update my table

--- backend/index.js
+++ backend/index.js
@@ -233,7 +233,7 @@ app.get("/items", async (req, res) => {
 // Add new item route
 app.post("/items", async (req, res) => {
   let { user_id, name, description, imageUrl, price, categories } = req.body;
-  user_id = 1;
+  user_id = 19;
   try {
     const client = await pool.connect();
 
